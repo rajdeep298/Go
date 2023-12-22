363273dd932df60ebb9df448747a350ffc84bc07
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-12-23 00:01:07 +0530

    some mild changes

--- 17-Structs.go
+++ 17-Structs.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
+func structure(x string) *person {
+	person1 := person{name: x}
+	person1.age = 42
+	return &person1
+}
+func main() {
+	fmt.Println(person{"Bob", 20})
+	fmt.Println(person{name: "Alice", age: 30})
+	fmt.Println(person{name: "Fred"})
+	fmt.Println(&person{name: "Ann", age: 40})
+	s := person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	s2 := structure("John")
+	fmt.Println(s2)
+
+	sp := &s // Structs are mutable.
+	fmt.Println(sp.age)
+	sp.age = 51
+	fmt.Println(sp.age)
 }
